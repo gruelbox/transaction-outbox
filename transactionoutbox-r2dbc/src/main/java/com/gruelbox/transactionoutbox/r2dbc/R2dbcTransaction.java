c0f40da70c9f201e1bfd0b900fd96b1137fd6197
Graham Crockford <badgerwithagun@users.noreply.github.com>
2020-06-07 16:01:41 +0100

    Mark as Beta

--- transactionoutbox-r2dbc/src/main/java/com/gruelbox/transactionoutbox/r2dbc/R2dbcTransaction.java
+++ transactionoutbox-r2dbc/src/main/java/com/gruelbox/transactionoutbox/r2dbc/R2dbcTransaction.java
@@ -1,8 +1,9 @@
 package com.gruelbox.transactionoutbox.r2dbc;
 
+import com.gruelbox.transactionoutbox.Beta;
 import com.gruelbox.transactionoutbox.spi.BaseTransaction;
 import io.r2dbc.spi.Connection;
 
 /** A transaction operating on a non-blocking R2DBC {@link Connection}. */
-@SuppressWarnings("WeakerAccess")
+@Beta
 public interface R2dbcTransaction extends BaseTransaction<Connection> {}
