436baed648605551d026467643167473a70b9a46
Graham Crockford <6483013+badgerwithagun@users.noreply.github.com>
2020-12-01 21:40:09 +0000

    Allow invocations to be wrapped. Useful for recreating threadlocal state. (#98)
    Co-authored-by: Graham Crockford <badgerwithagun@users.noreply.github.com>

--- transactionoutbox-core/src/test/java/com/gruelbox/transactionoutbox/acceptance/TestH2.java
+++ transactionoutbox-core/src/test/java/com/gruelbox/transactionoutbox/acceptance/TestH2.java
@@ -1,9 +1,19 @@
 package com.gruelbox.transactionoutbox.acceptance;
 
-import com.gruelbox.transactionoutbox.Dialect;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import com.gruelbox.transactionoutbox.*;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Test;
+import org.slf4j.MDC;
 
 @SuppressWarnings("WeakerAccess")
 class TestH2 extends AbstractAcceptanceTest {
+
+  static final ThreadLocal<Boolean> inWrappedInvocation = ThreadLocal.withInitial(() -> false);
 
   @Override
   protected ConnectionDetails connectionDetails() {
@@ -15,5 +25,93 @@ class TestH2 extends AbstractAcceptanceTest {
         .user("test")
         .password("test")
         .build();
+  }
+
+  @Test
+  final void wrapInvocations() throws InterruptedException {
+
+    CountDownLatch latch = new CountDownLatch(1);
+    TransactionManager transactionManager = simpleTxnManager();
+    TransactionOutbox outbox =
+        TransactionOutbox.builder()
+            .transactionManager(transactionManager)
+            .instantiator(
+                Instantiator.using(
+                    clazz ->
+                        (InterfaceProcessor)
+                            (foo, bar) -> {
+                              if (!inWrappedInvocation.get()) {
+                                throw new IllegalStateException("Not in a wrapped invocation");
+                              }
+                            }))
+            .listener(
+                new LatchListener(latch)
+                    .andThen(
+                        new TransactionOutboxListener() {
+                          @Override
+                          public void wrapInvocation(Invocator invocator)
+                              throws IllegalAccessException, IllegalArgumentException,
+                                  InvocationTargetException {
+                            inWrappedInvocation.set(true);
+                            try {
+                              invocator.run();
+                            } finally {
+                              inWrappedInvocation.remove();
+                            }
+                          }
+                        }))
+            .persistor(Persistor.forDialect(connectionDetails().dialect()))
+            .build();
+
+    outbox.initialize();
+    clearOutbox();
+
+    transactionManager.inTransaction(
+        () -> outbox.schedule(InterfaceProcessor.class).process(1, "bar"));
+    assertTrue(latch.await(5, TimeUnit.SECONDS));
+  }
+
+  @Test
+  final void wrapInvocationsWithMDC() throws InterruptedException {
+
+    CountDownLatch latch = new CountDownLatch(1);
+    TransactionManager transactionManager = simpleTxnManager();
+    TransactionOutbox outbox =
+        TransactionOutbox.builder()
+            .transactionManager(transactionManager)
+            .instantiator(
+                Instantiator.using(
+                    clazz ->
+                        (InterfaceProcessor)
+                            (foo, bar) -> {
+                              if (!Boolean.parseBoolean(MDC.get("BAR"))) {
+                                throw new IllegalStateException("Not in a wrapped invocation");
+                              }
+                            }))
+            .listener(
+                new LatchListener(latch)
+                    .andThen(
+                        new TransactionOutboxListener() {
+                          @Override
+                          public void wrapInvocation(Invocator invocator)
+                              throws IllegalAccessException, IllegalArgumentException,
+                                  InvocationTargetException {
+                            MDC.put("BAR", "true");
+                            try {
+                              invocator.run();
+                            } finally {
+                              MDC.remove("BAR");
+                            }
+                          }
+                        }))
+            .persistor(Persistor.forDialect(connectionDetails().dialect()))
+            .build();
+
+    outbox.initialize();
+    clearOutbox();
+
+    transactionManager.inTransaction(
+        () -> outbox.schedule(InterfaceProcessor.class).process(1, "bar"));
+    assertTrue(latch.await(5, TimeUnit.SECONDS));
   }
 }
