bd6bbdd664313af7bbb45d6399fdad926cb8cf4f
Graham Crockford <badgerwithagun@users.noreply.github.com>
2021-08-22 14:37:29 +0100

    Remove v2 build, fix postgres

--- transactionoutbox-core/src/main/java/com/gruelbox/transactionoutbox/sql/PostgreSqlDialect.java
+++ transactionoutbox-core/src/main/java/com/gruelbox/transactionoutbox/sql/PostgreSqlDialect.java
@@ -66,7 +66,7 @@ final class PostgreSqlDialect extends Dialect {
         + "  SELECT id FROM {{table}}"
         + "  WHERE nextAttemptTime < ? AND"
         + "        processed = true AND"
-        + "        blacklisted = false"
+        + "        blocked = false"
         + "  LIMIT ?)";
   }
 
