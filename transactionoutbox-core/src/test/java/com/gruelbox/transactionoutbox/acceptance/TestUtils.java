ffc284a50b568ceb8811a92f98ec31a251bee3b0
Andrew S <soandrew@users.noreply.github.com>
2021-10-08 10:30:05 -0400

    fix: DefaultPersistor should honour migrate configuration as per documentation (#184)

--- transactionoutbox-core/src/test/java/com/gruelbox/transactionoutbox/acceptance/TestUtils.java
+++ transactionoutbox-core/src/test/java/com/gruelbox/transactionoutbox/acceptance/TestUtils.java
@@ -3,10 +3,10 @@ package com.gruelbox.transactionoutbox.acceptance;
 import com.gruelbox.transactionoutbox.TransactionManager;
 import java.sql.Statement;
 
-class TestUtils {
+public class TestUtils {
 
   @SuppressWarnings("SameParameterValue")
-  static void runSql(TransactionManager transactionManager, String sql) {
+  public static void runSql(TransactionManager transactionManager, String sql) {
     transactionManager.inTransaction(
         tx -> {
           try {
