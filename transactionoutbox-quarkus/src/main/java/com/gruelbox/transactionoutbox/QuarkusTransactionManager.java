4b4891a4ead1f38857db69226dbf413c3378d7db
RomainWilbert <romain.wilbert@gmail.com>
2022-10-07 10:51:50 +0100

    Feature : Quarkus integration (#320)
    * feat: cdi implementation
    
    * fix: PR comments
    
    * fix: cdi transaction manager instantiation to be transaction aware
    
    * Update CdiTransactionManager.java
    
    * ref: module name (quarkus)
    
    * ref: module name
    
    Co-authored-by: Romain Wilbert <romain.wilbert-ext@pole-emploi.fr>
    
    (cherry picked from commit 121f27d1d36c5643b9eacaf6b4f673b8d2f5f806)

---
+++ transactionoutbox-quarkus/src/main/java/com/gruelbox/transactionoutbox/QuarkusTransactionManager.java
@@ -0,0 +1,52 @@
+package com.gruelbox.transactionoutbox;
+
+import com.gruelbox.transactionoutbox.jdbc.JdbcTransactionManager;
+import com.gruelbox.transactionoutbox.spi.*;
+import com.gruelbox.transactionoutbox.spi.ThrowingTransactionalSupplier;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.PreparedStatement;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import javax.transaction.TransactionSynchronizationRegistry;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+
+/** Transaction manager which uses cdi and quarkus. */
+@ApplicationScoped
+public class QuarkusTransactionManager implements JdbcTransactionManager<CdiTransaction>, InitializationEventPublisher {
+
+   private final CdiTransaction transactionInstance;
+
+   @Inject
+   public QuarkusTransactionManager(DataSource datasource, TransactionSynchronizationRegistry tsr) {
+      this.transactionInstance = new CdiTransaction(datasource, tsr);
+   }
+
+   @Override
+   @Transactional(value = TxType.REQUIRES_NEW)
+   public <T, E extends Exception> T inTransactionReturnsThrows(ThrowingTransactionalSupplier<T, E, CdiTransaction> work) throws E {
+      return work.doWork(transactionInstance);
+   }
+
+   @Override
+   public TransactionalInvocation extractTransaction(Method method, Object[] args) {
+      return TransactionManagerSupport.toTransactionalInvocation(method, args, transactionInstance);
+   }
+
+   @Override
+   public Invocation injectTransaction(Invocation invocation, CdiTransaction transaction) {
+      return TransactionManagerSupport.injectTransactionIntoInvocation(
+              invocation, Void.class, transaction);
+   }
+
+   @Override
+   public void onPublishInitializationEvents(InitializationEventBus eventBus) {
+      eventBus.sendEvent(
+              SerializableTypeRequired.class, new SerializableTypeRequired(CdiTransaction.class));
+   }
+}
---
+++ transactionoutbox-quarkus/src/test/java/com/gruelbox/transactionoutbox/acceptance/BusinessService.java
@@ -0,0 +1,25 @@
+package com.gruelbox.transactionoutbox.acceptance;
+
+import com.gruelbox.transactionoutbox.TransactionOutbox;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class BusinessService {
+  private DaoImpl dao;
+
+  @Inject private TransactionOutbox outbox;
+
+  @Inject
+  public BusinessService(DaoImpl dao) {
+    this.dao = dao;
+  }
+
+  @Transactional
+  public void writeSomeThingAndRemoteCall(String value, boolean throwException) {
+    dao.writeSomethingIntoDatabase(value);
+    RemoteCallService proxy = outbox.schedule(RemoteCallService.class);
+    proxy.callRemote(throwException);
+  }
+}
