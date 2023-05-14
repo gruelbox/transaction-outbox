32ff695f6b64829d9cb789a7e02b64ce8cd350a0
Graham Crockford <badgerwithagun@users.noreply.github.com>
2023-05-14 15:01:16 +0100

    Reduce logging in builds, fix Spring compilation

--- transactionoutbox-spring/src/test/java/com/gruelbox/transactionoutbox/acceptance/Customer.java
+++ transactionoutbox-spring/src/test/java/com/gruelbox/transactionoutbox/acceptance/Customer.java
@@ -3,10 +3,14 @@ package com.gruelbox.transactionoutbox.acceptance;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 class Customer {
   @Id private Long id;
   @Column private String firstName;
--- transactionoutbox-spring/src/test/java/com/gruelbox/transactionoutbox/acceptance/Event.java
+++ transactionoutbox-spring/src/test/java/com/gruelbox/transactionoutbox/acceptance/Event.java
@@ -4,10 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 class Event {
   @Id private Long id;
   @Column private String description;
