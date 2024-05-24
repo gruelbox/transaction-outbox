fc1291c4793a660dba70a9f4ea1ee85e642db69d
m1c1b <viaznin123@gmail.com>
2024-05-24 11:35:30 +0500

    A bit of refactoring.

--- transactionoutbox-core/src/main/java/com/gruelbox/transactionoutbox/spi/AbstractProxyFactory.java
+++ transactionoutbox-core/src/main/java/com/gruelbox/transactionoutbox/spi/AbstractProxyFactory.java
@@ -5,14 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import net.bytebuddy.TypeCache;
 import org.objenesis.Objenesis;
 import org.objenesis.ObjenesisStd;
-import org.objenesis.instantiator.ObjectInstantiator;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.function.BiFunction;
+
+import static java.lang.reflect.Proxy.newProxyInstance;
+import static java.util.Optional.ofNullable;
 
 /**
  * @author Ilya Viaznin
@@ -24,63 +24,6 @@ public abstract class AbstractProxyFactory {
 
     private final TypeCache<Class<?>> byteBuddyCache = setupByteBuddyCache();
 
-    @SuppressWarnings({"unchecked", "cast"})
-    public <T> T createProxy(Class<T> clazz, BiFunction<Method, Object[], T> processor) {
-        if (clazz.isInterface()) {
-            // Fastest - we can just proxy an interface directly
-            return (T) Proxy.newProxyInstance(
-                            clazz.getClassLoader(),
-                            new Class[]{clazz},
-                            (proxy, method, args) -> processor.apply(method, args));
-        }
-        else {
-            var proxy = buildByteBuddyProxyClass(clazz);
-            return constructProxy(clazz, processor, proxy);
-        }
-    }
-
-    protected abstract <T> Callable<Class<?>> byteBuddyProxyCallable(Class<T> clazz);
-
-    protected <T> T constructProxy(
-            Class<T> clazz, BiFunction<Method, Object[], T> processor, Class<? extends T> proxy) {
-        final T instance;
-        if (hasDefaultConstructor(clazz)) {
-            instance = Utils.uncheckedly(() -> proxy.getDeclaredConstructor().newInstance());
-        }
-        else {
-            if (objenesis == null) {
-                throw new MissingOptionalDependencyException("org.objenesis", "objenesis");
-            }
-            ObjectInstantiator<? extends T> instantiator = objenesis.getInstantiatorOf(proxy);
-            instance = instantiator.newInstance();
-        }
-        Utils.uncheck(
-                () -> {
-                    var field = instance.getClass().getDeclaredField("handler");
-                    field.set(
-                            instance,
-                            (InvocationHandler) (proxy1, method, args) -> processor.apply(method, args));
-                });
-        return instance;
-    }
-
-    protected void buddyCacheExistenceCheck() {
-        Optional.ofNullable(byteBuddyCache)
-                .orElseThrow(() -> new MissingOptionalDependencyException("net.bytebuddy", "byte-buddy"));
-    }
-
-    @SuppressWarnings({"unchecked", "cast"})
-    protected <T> Class<? extends T> buildByteBuddyProxyClass(Class<T> clazz) {
-        buddyCacheExistenceCheck();
-
-        return (Class<? extends T>)
-                byteBuddyCache.findOrInsert(
-                        clazz.getClassLoader(),
-                        clazz,
-                        byteBuddyProxyCallable(clazz)
-                );
-    }
-
     private static boolean hasDefaultConstructor(Class<?> clazz) {
         try {
             clazz.getConstructor();
@@ -90,12 +33,55 @@ public abstract class AbstractProxyFactory {
         }
     }
 
+    protected abstract <T> Callable<Class<?>> byteBuddyProxyCallable(Class<T> clazz);
+
+    @SuppressWarnings({"unchecked", "cast"})
+    public <T> T createProxy(Class<T> clazz, BiFunction<Method, Object[], T> processor) {
+        return clazz.isInterface()
+                // Fastest, we can just proxy an interface directly
+                ? (T) newProxyInstance(clazz.getClassLoader(),
+                                       new Class[]{clazz},
+                                       (proxy, method, args) -> processor.apply(method, args))
+                : constructProxy(clazz, processor, buildByteBuddyProxyClass(clazz));
+    }
+
+    protected <T> T constructProxy(Class<T> clazz, BiFunction<Method, Object[], T> processor, Class<? extends T> proxy) {
+        final T instance;
+        if (hasDefaultConstructor(clazz)) instance = Utils.uncheckedly(() -> proxy.getDeclaredConstructor().newInstance());
+        else {
+            if (objenesis == null) throw new MissingOptionalDependencyException("org.objenesis", "objenesis");
+
+            var instantiator = objenesis.getInstantiatorOf(proxy);
+            instance = instantiator.newInstance();
+        }
+        Utils.uncheck(
+                () -> {
+                    var field = instance.getClass().getDeclaredField("handler");
+                    field.set(
+                            instance,
+                            (InvocationHandler) (proxy1, method, args) -> processor.apply(method, args));
+                });
+
+        return instance;
+    }
+
+    @SuppressWarnings({"unchecked", "cast"})
+    protected <T> Class<? extends T> buildByteBuddyProxyClass(Class<T> clazz) {
+        return (Class<? extends T>)
+                ofNullable(byteBuddyCache)
+                        .orElseThrow(() -> new MissingOptionalDependencyException("net.bytebuddy", "byte-buddy"))
+                        .findOrInsert(
+                                clazz.getClassLoader(),
+                                clazz,
+                                byteBuddyProxyCallable(clazz)
+                        );
+    }
+
     private TypeCache<Class<?>> setupByteBuddyCache() {
         try {
             return new TypeCache<>(TypeCache.Sort.WEAK);
         } catch (NoClassDefFoundError error) {
-            log.info(
-                    "ByteBuddy is not on the classpath, so only interfaces can be used with transaction-outbox");
+            log.info("ByteBuddy is not on the classpath, so only interfaces can be used with transaction-outbox");
             return null;
         }
     }
@@ -104,8 +90,7 @@ public abstract class AbstractProxyFactory {
         try {
             return new ObjenesisStd();
         } catch (NoClassDefFoundError error) {
-            log.info(
-                    "Objenesis is not on the classpath, so only interfaces or classes with default constructors can be used with transaction-outbox");
+            log.info("Objenesis is not on the classpath, so only interfaces or classes with default constructors can be used with transaction-outbox");
             return null;
         }
     }
