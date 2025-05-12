779724ff0aa6cdd4176c019a4969c9d2e7bab199
Graham Crockford <6483013+badgerwithagun@users.noreply.github.com>
2025-05-12 13:07:18 +0100

    Update DefaultDialect.java

--- transactionoutbox-core/src/main/java/com/gruelbox/transactionoutbox/DefaultDialect.java
+++ transactionoutbox-core/src/main/java/com/gruelbox/transactionoutbox/DefaultDialect.java
@@ -158,6 +158,12 @@ class DefaultDialect implements Dialect {
               12,
               "Add flush index to support ordering",
               "CREATE INDEX IX_TXNO_OUTBOX_2 ON TXNO_OUTBOX (topic, processed, seq)"));
+      migrations.put(
+          13,
+          new Migration(
+              12,
+              "Enforce UTF8 collation",
+              null"));
     }
 
     Builder setMigration(Migration migration) {
