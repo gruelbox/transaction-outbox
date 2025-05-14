bb7cbc300f626de3926932cdf0cfa83a5a78808f
Stefan Kastenholz <stefan.kastenholz@gmail.com>
2025-05-14 08:16:32 +0200

    fix code formatting

--- transactionoutbox-core/src/main/java/com/gruelbox/transactionoutbox/DeserializationFailedException.java
+++ transactionoutbox-core/src/main/java/com/gruelbox/transactionoutbox/DeserializationFailedException.java
@@ -1,6 +1,5 @@
 package com.gruelbox.transactionoutbox;
 
-
 /** Thrown when de-serialization of an invocation fails. */
 class DeserializationFailedException extends RuntimeException {
   DeserializationFailedException(String message, Throwable cause) {
