b470204c673cfcb0f464751a3a34623d4547f99b
Graham Crockford <6483013+badgerwithagun@users.noreply.github.com>
2026-02-09 15:15:03 +0000

    Search for libc a bit more aggressively; this should also work with musl if that's what the Docker image uses

--- transactionoutbox-virtthreads/src/test/java/com/gruelbox/transactionoutbox/virtthreads/TestVirtualThreadsH2.java
+++ transactionoutbox-virtthreads/src/test/java/com/gruelbox/transactionoutbox/virtthreads/TestVirtualThreadsH2.java
@@ -6,51 +6,67 @@ import java.lang.foreign.*;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class TestVirtualThreadsH2 extends AbstractVirtualThreadsTest {
 
+  private static final String QSORT = "qsort";
+
+  /**
+   * Ensures that the logic we use in {@link AbstractVirtualThreadsTest} to detect thread pinning
+   * actually works. In Java 25 we have to turn ourselves inside out to achieve this; even {@code
+   * synchronized} doesn't pin a thread anymore. The only thing that seems to work is to make a
+   * native call that calls back to Java. When that stops working, this test can probably be
+   * removed; there will be very little likelihood of pinning in practice once that last thing is
+   * resolved in the JDK.
+   */
   @Test
   void forceTriggerPinningViaUpcall() throws Throwable {
     simulateThreadPin();
+
+    // Prevents the check on exit from throwing
     assertTrue(didPin());
   }
 
   private void simulateThreadPin() throws NoSuchMethodException, IllegalAccessException {
     Linker linker = Linker.nativeLinker();
-
-    // Find qsort (available in libc/msvcrt)
     SymbolLookup libc =
-        System.getProperty("os.name").toLowerCase().contains("win")
-            ? SymbolLookup.libraryLookup("msvcrt.dll", Arena.global())
-            : SymbolLookup.loaderLookup();
-
+        name -> {
+          try {
+            var lookup =
+                System.getProperty("os.name").toLowerCase().contains("win")
+                    ? SymbolLookup.libraryLookup("msvcrt.dll", Arena.global())
+                    : SymbolLookup.libraryLookup("libc.so.6", Arena.global());
+            return lookup.find(name);
+          } catch (Exception e) {
+            return Optional.empty();
+          }
+        };
     MethodHandle qsort =
         linker.downcallHandle(
-            libc.find("qsort").orElseThrow(),
+            libc.find(QSORT)
+                .or(() -> Linker.nativeLinker().defaultLookup().find(QSORT))
+                .or(() -> SymbolLookup.loaderLookup().find(QSORT))
+                .orElseThrow(() -> new RuntimeException("Could not find qsort")),
             FunctionDescriptor.ofVoid(
                 ValueLayout.ADDRESS,
                 ValueLayout.JAVA_LONG,
                 ValueLayout.JAVA_LONG,
                 ValueLayout.ADDRESS));
-
-    // Create an upcall to our Java 'block' method
     MethodHandle upcallTarget =
         MethodHandles.lookup()
             .findStatic(
                 getClass(),
                 "javaBlockCallback",
                 MethodType.methodType(int.class, MemorySegment.class, MemorySegment.class));
-
     MemorySegment upcallStub =
         linker.upcallStub(
             upcallTarget,
             FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.ADDRESS),
             Arena.global());
-
-    // Run in a virtual thread
     Thread.ofVirtual()
         .name("Pin-Me-Thread")
         .start(
